de2a675c1872b855259ea37612e9652ae08e7338
Sam Tobin-Hochstadt <samth@ccs.neu.edu>
2015-07-08 14:14:05 +0200

    Add tooltips.
    Closes #1.

--- main.rkt
+++ main.rkt
@@ -10,9 +10,29 @@
          [(and (null? args) (null? ids))
           (datum->syntax #'here `(#%app ,@(reverse result)) stx stx)]
          [(null? args)
-          (datum->syntax #'here `(lambda ,(reverse ids) ,(datum->syntax #'here `(#%app ,@(reverse result)) stx stx)) stx stx)]
+          (syntax-property
+           (datum->syntax #'here
+                          `(lambda ,(reverse ids)
+                             ,(datum->syntax #'here `(#%app ,@(reverse result)) stx stx))
+                          stx stx)
+           'mouse-over-tooltips
+           (vector
+            stx
+            (syntax-position stx)
+            (+ (syntax-position stx) (syntax-span stx))
+            (format "this application is automatically a function using _")))]
          [(and (identifier? (car args)) (free-identifier=? (car args) #'_))
-          (let ([tmp (syntax-track-origin (car (generate-temporaries '(_))) (car args) #'_)])
+          (let* ([_-id (car args)]
+                 [id (car (generate-temporaries '(_)))]
+                 [tmp
+                  (syntax-property 
+                   (syntax-track-origin
+                    id (car args) #'_)
+                   'mouse-over-tooltips
+                   (vector id
+                           (sub1 (syntax-position _-id))
+                           (sub1 (+ (syntax-position _-id) (syntax-span _-id)))
+                           "_ is the parameter to the anonymous function"))])
             (loop (cdr args) (cons tmp ids) (cons tmp result)))]
          [else
           (loop (cdr args) ids (cons (car args) result))]))]))
