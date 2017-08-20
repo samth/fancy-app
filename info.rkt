905f31ccdd1ee3f87be0333803b0db23f9db0d13
Jack Firth <jackhfirth@gmail.com>
2017-08-19 23:00:33 -0700

    Add docs
    Includes lightweight tests with eval:check

--- info.rkt
+++ info.rkt
@@ -1,4 +1,9 @@
 #lang info
 
 (define collection "fancy-app")
+(define scribblings
+  '(("main.scrbl" () (library) "fancy-app")))
 (define deps '("base"))
+(define build-deps
+  '("racket-doc"
+    "scribble-lib"))
