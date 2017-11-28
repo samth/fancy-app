260bcf4dd03e372506e1febbc9157f92e21a1348
AlexKnauth <alexander@knauth.org>
2017-11-28 17:18:10 -0500

    depend on scribble-lib version 1.16
    for scribble/example

--- info.rkt
+++ info.rkt
@@ -3,8 +3,10 @@
 (define collection "fancy-app")
 (define scribblings
   '(("main.scrbl" () (library) "fancy-app")))
+
 (define deps '("base"))
+
 (define build-deps
   '("rackunit-lib"
     "racket-doc"
-    "scribble-lib"))
+    ["scribble-lib" #:version "1.16"]))
